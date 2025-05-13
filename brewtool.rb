c6f8c1bd13e2d497f1951c631eb8486fd971d367
Marko Mijacevic <138769768+marko0905@users.noreply.github.com>
2025-05-13 23:03:58 +0200

    Update brewtool.rb

--- brewtool.rb
+++ brewtool.rb
@@ -1,8 +1,8 @@
 class Brewtool < Formula
   desc "Terminal user interface (TUI) for Homebrew package management"
   homepage "https://github.com/marko0905/brewtool"
-  url "https://github.com/marko0905/brewtool/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "f6843cca24cdb73219edbbca061e26d503ac72cae31942202719bdb624e7e3de"
+  url "https://github.com/marko0905/brewtool/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "18978f492dd59e05448a834a1a4af4ca5760800097cf77bb23f08e4007d6133a"
   license "MIT"
 
   depends_on "bun"
